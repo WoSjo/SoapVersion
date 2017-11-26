017dfe4115e85abd5ba10dc2c2ffeafd07842c2b
Wouter van Marrum <wouter.van.marrum@protonmail.com>
2017-11-26 23:31:37 +0100

    - Updated `SoapServersController` controller. - Updated `SoapServer` model.

--- app/Http/Controllers/Dashboard/SoapServersController.php
+++ app/Http/Controllers/Dashboard/SoapServersController.php
@@ -3,33 +3,36 @@
 namespace SoapVersion\Http\Controllers\Dashboard;
 
 use Illuminate\Http\Request;
+use SoapVersion\Http\Controllers\Controller;
 use SoapVersion\Models\Dashboard\SoapServer;
 
 class SoapServersController extends Controller
 {
     public function index()
     {
-        //
+        $soapServers = SoapServer::byUserId();
+
+        return view('dashboard.soap_servers.index', compact('soapServers'));
     }
 
     public function create()
     {
-        //
+        return view('dashboard.soap_servers.create');
     }
 
     public function store(Request $request)
     {
-        //
+        // Create new soap server
     }
 
-    public function show(SoapServer $soapServers)
+    public function show(SoapServer $soapServer)
     {
-        //
+        return view('dashboard.soap_servers.show', compact('soapServer'));
     }
 
-    public function edit(SoapServer $soapServers)
+    public function edit(SoapServer $soapServer)
     {
-        //
+        return view('dashboard.soap_servers.edit', compact('soapServer'));
     }
 
     public function update(Request $request, SoapServer $soapServers)
--- app/Models/Dashboard/SoapServer.php
+++ app/Models/Dashboard/SoapServer.php
@@ -2,9 +2,72 @@
 
 namespace SoapVersion\Models\Dashboard;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Crypt;
+use SoapVersion\Models\User;
 
 class SoapServer extends Model
 {
-    //
+    /** @var array */
+    protected $fillable = [
+        'name',
+        'slug',
+        'host',
+        'port'
+    ];
+
+    /** @var array */
+    protected $dates = [
+        'deleted_at'
+    ];
+
+    /** @var array */
+    protected $casts = [
+        'name' => 'string',
+        'slug' => 'string',
+        'host' => 'string',
+        'port' => 'integer',
+    ];
+
+    /**
+     * @param string $slug
+     */
+    public function setSlugAttribute(string $slug): void
+    {
+        $this->attributes['slug'] = str_slug($slug);
+    }
+
+    /**
+     * @param string $host
+     */
+    public function setHostAttribute(string $host): void
+    {
+        $this->attributes['host'] = encrypt($host);
+    }
+
+    /**
+     * @return string
+     */
+    public function getHostAttribute(): string
+    {
+        return decrypt($this->host);
+    }
+
+    /**
+     * @param Builder $query
+     * @return \Illuminate\Database\Eloquent\Collection|static[]
+     */
+    public function scopeByUserId(Builder $query)
+    {
+        return $query->has('user')->get();
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
