118b8b70c5414f90fd22b6bef5be863fa63739dc
Wouter van Marrum <wouter.van.marrum@protonmail.com>
2017-11-26 23:35:53 +0100

    - Added lanuage files.  - soap_server (nl, en)  - utility (nl)

---
+++ resources/lang/en/soap_server.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'soap server' => 'soap server|soap servers',
+    'for account' => 'for account',
+    'name' => 'name',
+    'slug' => 'slug',
+    'host' => 'host',
+    'port' => 'port'
+];
---
+++ resources/lang/nl/utility.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'actions' => 'acties',
+    'new record' => 'nieuw record voor :type',
+    'no records found' => 'geen records gevonden',
+];
