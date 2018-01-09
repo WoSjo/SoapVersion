eeaad567f7933b0ed41af77d3fcb635a4c07a40b
Wouter van Marrum <wouter.van.marrum@protonmail.com>
2018-01-09 21:06:22 +0100

    - Updated `web` routes ( added group with auth middleware )

--- routes/web.php
+++ routes/web.php
@@ -13,20 +13,22 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::group(['middleware' => ['auth'], 'namespace' => 'Servers'], function () {
+Route::group(['middleware' => ['auth']], function () {
+    Route::group(['namespace' => 'Servers'], function () {
 
-    Route::resource('servers', 'ServerController');
+        Route::resource('servers', 'ServerController');
 
-    Route::group(['prefix' => '{server}', 'as' => 'servers.'], function () {
-        Route::resource('endpoints', 'EndpointController');
+        Route::group(['prefix' => '{server}', 'as' => 'servers.'], function () {
+            Route::resource('endpoints', 'EndpointController');
+        });
     });
-});
 
-Route::group(['prefix' => '{endpoint}', 'as' => 'endpoints.'], function () {
-    Route::resource('versions', 'VersionController');
-    Route::get('versions/{version}/mail', function ($endpoint, $version) {
-        $endpoint = \SoapVersion\Models\Server\Endpoint::find($endpoint);
-        $version = Version::find($version);
-        return new EndpointDifferenceFound($endpoint, $version);
-    })->name('versions.mail');
+    Route::group(['prefix' => '{endpoint}', 'as' => 'endpoints.'], function () {
+        Route::resource('versions', 'VersionController');
+        Route::get('versions/{version}/mail', function ($endpoint, $version) {
+            $endpoint = \SoapVersion\Models\Server\Endpoint::find($endpoint);
+            $version = Version::find($version);
+            return new EndpointDifferenceFound($endpoint, $version);
+        })->name('versions.mail');
+    });
 });
