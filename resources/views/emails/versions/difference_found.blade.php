095a6b5891fbad721892aae59b2407f0b0b39f4c
Wouter van Marrum <wouter.van.marrum@protonmail.com>
2018-01-10 22:15:35 +0100

    WIP - Changed a lot of stuff.

--- resources/views/emails/versions/difference_found.blade.php
+++ resources/views/emails/versions/difference_found.blade.php
@@ -1,15 +1,15 @@
 @component('mail::message')
 
-    # {{ $headerText }}.
+# {{ $headerText }}.
 
-    @if ($viewButton)
-        @component('mail::button', ['url' => route('endpoints.versions.show', [$endpoint, $version])])
-            {{ __('utility.view') }} {{ trans_choice('version.choice', 1) }}
-        @endcomponent
-    @endif
+@if ($viewButton)
+    @component('mail::button', ['url' => route('endpoints.versions.show', [$endpoint, $version])])
+        {{ __('utility.view') }} {{ trans_choice('version.choice', 1) }}
+    @endcomponent
+@endif
 
-    {!! $difference !!}
-    <br>
-    Thanks,<br>
-    {{ config('app.name') }}
+{!! $difference !!}
+<br>
+Thanks,<br>
+{{ config('app.name') }}
 @endcomponent
